6759b696c53c807346ef47b3025fe3e6438fdec0
jasmjosa382 <jasmjosa382@ed.amdsb.ca>
2022-11-11 11:27:30 -0500

    save

--- MethodsPractice/Form1.cs
+++ MethodsPractice/Form1.cs
@@ -22,6 +22,10 @@ namespace MethodsPractice
             /// TODO - Place method calls here
 
             PrintSignature();
+            RectangleArea();
+            Addition();
+            Area();
+            Mode();
 
         }
 
@@ -39,21 +43,49 @@ namespace MethodsPractice
 
         public void RectangleArea()
         {
+            int width, length, area;
 
+            width = 5;
+            length = 7;
+
+            area = width * length;
+
+            q2Output.Text = $"The area of a {width} x {length} rectangle is {area} units squared";
         }
 
         /// 3. Create a method to get numbers from q3Num1Input and q3Num2Input,
         /// add them together, and then display output as follows:
         /// 
         /// 5 + 4 = 9 
+        public void Addition()
+        {
+            int num1, num2, sum;
+
+            num1 = Convert.ToInt16(q3Num1Input.Text);
+            num2 = Convert.ToInt16(q3Num2Input.Text);
+
+            sum = num1 + num2;
+
+            q3Output.Text = $"{num1} + {num2} = {sum}";
+
+        }
 
 
-        
         /// 4. Create a method to calculate the area of a circle based on  
         /// the radius entered in radiusInput, then display output as follows:
         /// 
         /// The area of a circle with radius of 4 is 50.24 units squared
+        public void Area()
+        {
+            int radius;
+            double pi, area;
 
+            radius = Convert.ToInt16(radiusInput.Text);
+            pi = 3.14;
+            area = (radius * radius) * pi;
+
+            q4Output.Text = $"The area of a circle with a radius of {radius} is {area} units squared";
+        }
 
 
         /// 5. 3U Only - Create a method that will determine and display
@@ -63,6 +95,45 @@ namespace MethodsPractice
         /// Inputs: 5, 7, 2
         /// he largest value is 7
 
+        public void Mode()
+        {
+            int num1 = Convert.ToInt32(q5Num1Input.Text);
+            int num2 = Convert.ToInt32(q5Num2Input.Text);
+            int num3 = Convert.ToInt32(q5Num3Input.Text);
 
+            int max = 0;
+
+            //    max = num1;
+            //    if (num2 > max)
+            //    {
+            //        max = num2;
+            //    }
+            //    else (num3 > max)
+            //    {
+            //        max = num3;
+            //    }
+
+            //    q5Output.Text = $"The largest value is {max}";
+
+
+            if (max == num1)
+            {
+                 q5Output.Text = $"The largest value is {max}";
+            }
+            else if (num2 > max)
+            {
+                max = num2;
+                q5Output.Text = $"The largest value is {max}";
+            }
+            else if (num3 > max)
+            {
+                max = num3;
+                q5Output.Text = $"The largest value is {max}";
+            }
+            else
+            {
+                q5Output.Text = $"All the values are equal";
+            }
+        }
     }
 }
